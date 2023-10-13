29880ac594f6eed67044e06fa1b9f9f3aef87807
Dayton Kotas <Kotasdm@gmatc.matc.edu>
2023-10-12 21:14:09 -0500

    Completed Activity 5

---
+++ API/Controllers/PostsController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public PostsController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        //Get api/posts
+        [HttpGet(Name = "GetPosts")]
+        public ActionResult<List<Post>> Get()
+        {
+            return this.context.Posts.ToList();
+        }
+    }
+}
